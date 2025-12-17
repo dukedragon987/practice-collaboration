16e24b2110a6869ee45a15c4384b111f5ad95ec6
dukedragon987 <ljg.moodie@gmail.com>
2025-12-17 12:57:02 -0500

    added int doodle

--- App.java
+++ App.java
@@ -1,3 +1,10 @@
-public class App {
-    
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+public class App extends Application{
+    int doodle;
+    public static void main(String[] args){
+        launch(args);
+    }
+   
 }
